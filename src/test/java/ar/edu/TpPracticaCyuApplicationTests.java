4769333bd92ed905c52c6f9125ddc0e749411f3b
Yazmin2511 <65320026+Yazmin2511@users.noreply.github.com>
2024-02-01 22:44:43 -0300

    Resubiendo

---
+++ src/test/java/ar/edu/TpPracticaCyuApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpPracticaCyuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
